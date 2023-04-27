49a9b36f0e6105a76d98f4c6def1778e156ef26c
Kimberly Sams-Carmona <97557502+KimSamsCarmona@users.noreply.github.com>
2023-04-27 00:39:59 -0500

    setup login routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script>
+	import '../app.postcss';
+	export let data;
+</script>
+
+<div class="min-h-full">
+	<nav class="navbar bg-base-100 border-b">
+		<div class="flex-1">
+			<a class="btn btn-ghost normal-case text-xl" href="/">COYERLand</a>
+		</div>
+		<div class="flex-none">
+			{#if !data.user}
+				<div class="dropdown dropdown-end">
+					<a href="/login" class="btn btn-primary">Login</a>
+					<a href="/register" class="btn btn-primary">Register</a>
+				</div>
+			{:else}
+				<div class="dropdown dropdown-end mr-4">
+					<a href="/play" class="btn btn-primary btn-outline">Play Game!</a>
+				</div>
+				<div class="dropdown dropdown-end">
+					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+					<!-- svelte-ignore a11y-label-has-associated-control -->
+					<label tabindex="0" class="btn btn-ghost btn-circle avatar">
+						<div class="w-10 rounded-full">
+							<img src="https://placeimg.com/80/80/people" alt="user avatar" />
+						</div>
+					</label>
+					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+					<ul
+						tabindex="0"
+						class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+					>
+						<li>
+							<a href="/my/books" class="justify-between">Book Log</a>
+						</li>
+						<li>
+							<a href="/my/settings">Settings</a>
+						</li>
+						<li>
+							<form action="/logout" method="POST">
+								<button type="submit" class="w-full text-start">Logout</button>
+							</form>
+						</li>
+					</ul>
+				</div>
+			{/if}
+		</div>
+	</nav>
+	<div class="py-10">
+		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+			<slot />
+		</div>
+	</div>
+</div>
