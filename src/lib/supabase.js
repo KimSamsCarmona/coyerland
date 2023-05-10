c183ccdca56f25c0d50f9f8f2d5f8fe5b41708c2
Kimberly Sams-Carmona <97557502+KimSamsCarmona@users.noreply.github.com>
2023-05-09 21:04:07 -0500

    Revert "imports & create supabase client"
    This reverts commit 2d5f461e95a455d8a98f98cd37e6c1060db1328f.

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabaseClient = createClient(
-	'https://zcoeclocmqljsqxnmdlo.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpjb2VjbG9jbXFsanNxeG5tZGxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMzODE5MjAsImV4cCI6MTk5ODk1NzkyMH0.OkkxlEMeJWoGeKt_Is2pHQJc9RWwxWVYnIuTavVhxtY'
-);
